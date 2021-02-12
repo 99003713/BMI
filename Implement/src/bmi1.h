33b522ed24b3d32202b5139eb3999aeb1d9524ff
Ajeet mishra <78849047+ajeetmishra-ltts@users.noreply.github.com>
2021-02-12 16:36:41 +0530

    Add files via upload

---
+++ Implement/src/bmi1.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "height.h"
+using namespace std;
+int bmi1(int w, int h)
+{   int bmi;
+    bmi = (w) / (h * h); //formulae used for BMI calculator.//
+    cout << "BMI REPORT : " << bmi << endl;
+    if (bmi < 18.5)
+        cout << "Result : Underweight";
+    else if (bmi < 18.5 && bmi > 24.9)
+        cout << "Result : Normal";
+    else
+        cout << "Result : Overweight";
+        height();
+        return 0;
+}
+
