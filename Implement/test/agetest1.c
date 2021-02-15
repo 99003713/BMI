65d08a01169a4bb893a210f8511947385e834740
Ajeet mishra <78849047+ajeetmishra-ltts@users.noreply.github.com>
2021-02-15 09:40:04 +0530

    Add files via upload

---
+++ Implement/test/agetest1.c
@@ -0,0 +1,20 @@
+#include "unity.h"
+#include "age.h"
+#include "test_division.h"
+
+void test_division_basicsa(void)
+{
+	TEST_ASSERT_EQUAL( 22 Years And 5 Months, subtraction(2020-(5+1),subtraction(12, 7));
+}
+
+int test_division(void)
+{
+	/* Initiate the Unity Test Framework */
+	UNITY_BEGIN();
+
+	/* Run Test functions */
+	RUN_TEST(test_division_basic);
+
+	/* Close the Unity Test Framework */
+	return UNITY_END();
+}
